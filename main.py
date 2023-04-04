5492bd4b2bd421420020dedf9b45ab20297ae948
opencv <152051@csgaugustinus.nl>
2023-04-04 20:57:55 +0200

    afgerond naar 10tallen + nieuw bord alleen dictionairy nog naar goede grote

--- main.py
+++ main.py
@@ -1,18 +1,107 @@
+import cv2 as cv
 import numpy as np
-import cv2
-import glob
-# termination criteria
-img = cv2.imread("sodoku.jpg")
-rows,cols,ch = img.shape
-print(img.shape)
 
-pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
-pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
+upper_red = np.array([77, 108, 219])  # BGR-code of your lowest red
+lower_red = np.array([53, 77, 182])  # BGR-code of your highest red
 
-M = cv2.getPerspectiveTransform(pts1,pts2)
+vid = cv.VideoCapture(0)
+chessboardSize = (13,8)
 
-dst = cv2.warpPerspective(img,M,(300,300))
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-plt.subplot(121),plt.imshow(img),plt.title('Input')
-plt.subplot(122),plt.imshow(dst),plt.title('Output')
-plt.show()
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
+objp[:,:2] = np.mgrid[0:chessboardSize[0],0:chessboardSize[1]].T.reshape(-1,2)
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+newCoords = []
+
+
+while (True):
+    ret, frame = vid.read()
+    ret, hoeken = vid.read()
+    #cv.imshow('frame', frame)
+
+    gray = cv.cvtColor(hoeken, cv.COLOR_BGR2GRAY)
+    ret, corners = cv.findChessboardCorners(gray, chessboardSize, None)
+    # If found, add object points, image points (after refining them)
+
+    if ret == True:
+
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+        imgpoints.append(corners)
+        newCorners = []
+        corners = corners.tolist()
+
+        for c in corners:
+            newCorners.append(c[0])
+
+        rounded_corners = []
+        for c in newCorners:
+            rounded_corners.append([round(c[0]), round(c[1])])
+
+        # Draw and display the corners
+        cv.drawChessboardCorners(hoeken, chessboardSize, corners2, ret)
+        cv.imshow('img', hoeken)
+
+
+        mask = cv.inRange(frame, lower_red, upper_red)
+        # get all non zero values
+        coords = cv.findNonZero(mask)
+        output = cv.bitwise_and(frame, frame, mask=mask)
+
+        center = [0, 0]
+        if coords is not None:
+            coords = coords.tolist()
+
+            for c in coords:
+                #round = [round(num) for num in coords]
+                newCoords.append(c[0])
+
+            center = [sum(c) / len(c) for c in zip(*newCoords)]
+            cv.circle(output, (int(center[0]), int(center[1])), 3, [0, 255, 0], 3)
+
+            # show the images
+        cv.imshow("images", np.hstack([frame, output]))
+        print(f'center: {center}')
+        sortedlist = sorted(rounded_corners, key=lambda k: [k[1], k[0]])
+        # print(f'List: {sortedlist}')
+
+        oneliner_corners = [[round(x / 10) * 10 for x in c] for c in rounded_corners]
+        cornersend = sorted(oneliner_corners, key=lambda k: [k[1], k[0]])
+        print(f'newCorners: {cornersend}')
+
+        # Creating an empty dictionary
+        dicts = {}
+        keys = range(len(sortedlist))
+        values = cornersend
+
+        for y in range(7):
+            for i in range(9):
+                if i < 8 and y < 6:
+                    point = values[(y * 9 + i)]
+                    point2 = values[(y * 9 + i + 10)]
+                    dicts[(i, y)] = point, point2
+
+        print(dicts)
+
+
+        for key in dicts:
+                if dicts[key][0][0] <= center[0] <= dicts[key][1][0] and dicts[key][0][1] <= center[1] <= dicts[key][1][1]:
+                    print(f"Punt is in gebied: {key}")
+
+        # if center != 0:
+        #     punt = center
+        #
+        #     for key in dicts:
+        #         if dicts[key][0][0] <= punt[0] <= dicts[key][1][0] and dicts[key][0][1] <= punt[1] <= dicts[key][1][1]:
+        #             print(f"Punt is in gebied: {key}")
+        cv.waitKey(1000)
+
+#coordinaten
+
+
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
--- oefenen/berekening.py
+++ oefenen/berekening.py
@@ -1,30 +1,24 @@
-import numpy as np
-
-# 2d array to list
-arr_2 = np.array([[0, 0], [2, 0], [1, 0], [4, 1], [4, 0],
-                  [0, 1], [1, 1], [2, 1], [3, 1], [3, 0],
-                  [0, 2], [1, 2], [2, 2], [3, 2], [4, 2]])
-list_2 = arr_2.tolist()
-print(list_2)
-sortedlist = sorted(list_2 , key=lambda k: [k[1], k[0]])
-print(f'List: {sortedlist}')
-
-# Creating an empty dictionary
 dicts = {}
-keys = range(len(sortedlist))
-values = sortedlist
-
-for y in range(3):
-        for i in range(5):
-                if i < 4 and y < 2:
-                        point = values[(y * 5 + i)]
-                        point2 = values[(y*5 + i +6)]
-                        dicts[(i, y)] = point, point2
-
-print(dicts)
-
 leuk = [[237, 311], [239, 271], [241, 232], [243, 193], [245, 154], [247, 116], [249, 78], [277, 312], [279, 272], [281, 232], [283, 193], [284, 155], [286, 116], [287, 78], [318, 314], [320, 273], [321, 233], [322, 194], [324, 155], [325, 117], [326, 78], [358, 314], [360, 274], [361, 234], [362, 195], [363, 156], [364, 117], [366, 78], [400, 315], [401, 275], [402, 195], [402, 235], [403, 156], [404, 117], [405, 78], [441, 316], [442, 235], [442, 275], [443, 195], [444, 156], [445, 116], [446, 78], [484, 235], [484, 276], [484, 317], [485, 155], [485, 195], [486, 77], [486, 116], [527, 155], [527, 195], [527, 236], [527, 277], [527, 318], [528, 76], [528, 115], [570, 75], [570, 115], [570, 155], [570, 195], [570, 236], [570, 277], [570, 319]]
 
 oneliner_corners = [[round(x/10)*10 for x in c] for c in leuk]
-sortedlist2 = sorted(oneliner_corners , key=lambda k: [k[0], k[1]])
-print(f'List: {sortedlist2}')
+sortedlist2 = sorted(oneliner_corners , key=lambda k: [k[1], k[0]])
+
+#print(f'List: {sortedlist2}')
+
+keys = range(len(sortedlist2))
+values = sortedlist2
+
+for y in range(7):
+    for i in range(9):
+        if i < 8 and y < 6:
+          point = values[(y * 9 + i)]
+          point2 = values[(y*9 + i + 10)]
+          dicts[(i, y)] = point, point2
+
+print(dicts)
+punt = [380, 243]
+
+for key in dicts:
+    if dicts[key][0][0] <= punt[0] <= dicts[key][1][0] and dicts[key][0][1] <= punt[1] <= dicts[key][1][1]:
+        print(f"Punt is in gebied: {key}")
